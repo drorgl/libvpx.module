a3bc3a8baa78ccc2992a6f3afad9f4187c74cded
scherkus@chromium.org <scherkus@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2010-05-19 19:36:09 +0000

    Checking in more libvpx stuff from Tom Finegan (tomfinegan@google.com).
    BUG=none
    TEST=none
    
    git-svn-id: http://src.chromium.org/svn/trunk/deps/third_party/libvpx@47701 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

---
+++ source/config/mac/ia32/vpx_config.c
@@ -0,0 +1,2 @@
+static const char* const cfg = "--disable-codecs --enable-vp8-decoder --disable-install-docs --disable-examples --disable-install-srcs --disable-postproc --target=x86-darwin9-gcc";
+const char *vpx_codec_build_config(void) {return cfg;}
---
+++ source/config/win/ia32/vpx_config.c
@@ -0,0 +1,2 @@
+static const char* const cfg = "--disable-codecs --enable-vp8-decoder --target=x86-win32-gcc --disable-postproc --disable-install-docs --disable-install-srcs --disable-examples";
+const char *vpx_codec_build_config(void) {return cfg;}
