59d3a75dfbddae8f545cb6b736daa95982fc41a2
scherkus@chromium.org <scherkus@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2010-05-20 19:14:50 +0000

    Add libvpx binaries for arm and arm-neon built by Johann Koenigg.
    Built from git://review.webmproject.org/libvpx @ 0ea50ce9cb4b65eee6afa1d041fe8beb5abda667, which matches existing libvpx binaries. 
    
    Patch by Tom Finegan (tomfinegan@google.com).
    
    BUG=none
    TEST=none
    
    
    git-svn-id: http://src.chromium.org/svn/trunk/deps/third_party/libvpx@47824 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

---
+++ source/config/linux/arm-neon/vpx_config.c
@@ -0,0 +1,2 @@
+static const char* const cfg = "--target=armv7-linux-gcc --disable-vp8-encoder --disable-postproc";
+const char *vpx_codec_build_config(void) {return cfg;}
---
+++ source/config/linux/arm/vpx_config.c
@@ -0,0 +1,2 @@
+static const char* const cfg = "--target=armv6-linux-gcc --disable-vp8-encoder --disable-postproc";
+const char *vpx_codec_build_config(void) {return cfg;}
