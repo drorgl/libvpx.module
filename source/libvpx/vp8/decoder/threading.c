5d9492ef7c0067bd2462bef873b1508f9882f2d8
tomfinegan@google.com <tomfinegan@google.com@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2010-09-01 05:18:36 +0000

    Update libvpx for chrome 7.  Fixes Lucid thread performance bug, and adds additional performance improvements for ia32 targets.  Mac and linux libvpx builds built with icc v11.1.  Windows build built with gcc v4.2.
    BUG=none
    TEST=Multithread decode should be faster than single thread decode on gLucid
    
    Review URL: http://codereview.chromium.org/3309001
    
    git-svn-id: http://src.chromium.org/svn/trunk/deps/third_party/libvpx@58133 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- source/libvpx/vp8/decoder/threading.c
+++ source/libvpx/vp8/decoder/threading.c
@@ -157,7 +157,7 @@ THREAD_FUNCTION vp8_thread_decoding_proc(void *p_data)
     int ithread = ((DECODETHREAD_DATA *)p_data)->ithread;
     VP8D_COMP *pbi = (VP8D_COMP *)(((DECODETHREAD_DATA *)p_data)->ptr1);
     MB_ROW_DEC *mbrd = (MB_ROW_DEC *)(((DECODETHREAD_DATA *)p_data)->ptr2);
-    ENTROPY_CONTEXT mb_row_left_context[4][4];
+    ENTROPY_CONTEXT_PLANES mb_row_left_context;
 
     while (1)
     {
@@ -197,12 +197,9 @@ THREAD_FUNCTION vp8_thread_decoding_proc(void *p_data)
                     recon_uvoffset = mb_row * recon_uv_stride * 8;
                     // reset above block coeffs
 
-                    xd->above_context[Y1CONTEXT] = pc->above_context[Y1CONTEXT];
-                    xd->above_context[UCONTEXT ] = pc->above_context[UCONTEXT];
-                    xd->above_context[VCONTEXT ] = pc->above_context[VCONTEXT];
-                    xd->above_context[Y2CONTEXT] = pc->above_context[Y2CONTEXT];
-                    xd->left_context = mb_row_left_context;
-                    vpx_memset(mb_row_left_context, 0, sizeof(mb_row_left_context));
+                    xd->above_context = pc->above_context;
+                    xd->left_context = &mb_row_left_context;
+                    vpx_memset(&mb_row_left_context, 0, sizeof(mb_row_left_context));
                     xd->up_available = (mb_row != 0);
 
                     xd->mb_to_top_edge = -((mb_row * 16)) << 3;
@@ -260,10 +257,7 @@ THREAD_FUNCTION vp8_thread_decoding_proc(void *p_data)
 
                         ++xd->mode_info_context;  /* next mb */
 
-                        xd->above_context[Y1CONTEXT] += 4;
-                        xd->above_context[UCONTEXT ] += 2;
-                        xd->above_context[VCONTEXT ] += 2;
-                        xd->above_context[Y2CONTEXT] ++;
+                        xd->above_context++;
 
                         //pbi->mb_row_di[ithread].current_mb_col = mb_col;
                         pbi->current_mb_col[mb_row] = mb_col;
@@ -604,15 +598,12 @@ void vp8_mtdecode_mb_rows(VP8D_COMP *pbi,
             if (mb_row > 0)
                 last_row_current_mb_col = &pbi->current_mb_col[mb_row -1];
 
-            vpx_memset(pc->left_context, 0, sizeof(pc->left_context));
+            vpx_memset(&pc->left_context, 0, sizeof(pc->left_context));
             recon_yoffset = mb_row * recon_y_stride * 16;
             recon_uvoffset = mb_row * recon_uv_stride * 8;
             // reset above block coeffs
 
-            xd->above_context[Y1CONTEXT] = pc->above_context[Y1CONTEXT];
-            xd->above_context[UCONTEXT ] = pc->above_context[UCONTEXT];
-            xd->above_context[VCONTEXT ] = pc->above_context[VCONTEXT];
-            xd->above_context[Y2CONTEXT] = pc->above_context[Y2CONTEXT];
+            xd->above_context = pc->above_context;
             xd->up_available = (mb_row != 0);
 
             xd->mb_to_top_edge = -((mb_row * 16)) << 3;
@@ -620,15 +611,12 @@ void vp8_mtdecode_mb_rows(VP8D_COMP *pbi,
 
             for (mb_col = 0; mb_col < pc->mb_cols; mb_col++)
             {
-
                 if ( mb_row > 0 && (mb_col & 7) == 0){
-               //if ( mb_row > 0 ){
-                  while (mb_col > (*last_row_current_mb_col - 8) && *last_row_current_mb_col != pc->mb_cols - 1)
-                  {
-                      x86_pause_hint();
-                      thread_sleep(0);
-                  }
-
+                    while (mb_col > (*last_row_current_mb_col - 8) && *last_row_current_mb_col != pc->mb_cols - 1)
+                    {
+                        x86_pause_hint();
+                        thread_sleep(0);
+                    }
                 }
 
                 if (xd->mode_info_context->mbmi.mode == SPLITMV || xd->mode_info_context->mbmi.mode == B_PRED)
@@ -672,10 +660,7 @@ void vp8_mtdecode_mb_rows(VP8D_COMP *pbi,
 
                 ++xd->mode_info_context;  /* next mb */
 
-                xd->above_context[Y1CONTEXT] += 4;
-                xd->above_context[UCONTEXT ] += 2;
-                xd->above_context[VCONTEXT ] += 2;
-                xd->above_context[Y2CONTEXT] ++;
+                xd->above_context++;
 
                 //pbi->current_mb_col_main = mb_col;
                 pbi->current_mb_col[mb_row] = mb_col;
@@ -774,7 +759,6 @@ void vp8_mt_loop_filter_frame( VP8D_COMP *pbi)
         for (mb_col = 0; mb_col < cm->mb_cols; mb_col++)
         {
             int Segment = (alt_flt_enabled) ? mbd->mode_info_context->mbmi.segment_id : 0;
-
 
             if ( mb_row > 0 && (mb_col & 7) == 0){
             // if ( mb_row > 0 ){
