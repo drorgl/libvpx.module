8c2b994b96f2c62efe2c2bb7fae249f2af513d49
tomfinegan@google.com <tomfinegan@google.com@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2011-04-07 15:11:56 +0000

    Update libvpx sources to v0.9.6-94-g82315be.
    This is the Bali release of libvpx plus bug fixes and 
    additional performance improvements.  Taken from a clone of
    the review.webmproject.org/libvpx master branch at commit 
    hash 82315be75d54d00ad5f2ea51f844f27f2d921dc0.
    
    Library builds are included for:
    
    - linux (arm, arm-neon, ia32, and x64)
    - mac (ia32)
    - windows (ia32)
    
    NOTE: GYP modification required for linux will come in a 
    later commit; before the libvpx_revision value is DEPS is
    rolled forward.
    
    BUG=69288
    TEST=none
    Review URL: http://codereview.chromium.org/6745004
    
    git-svn-id: http://src.chromium.org/svn/trunk/deps/third_party/libvpx@80779 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- source/config/linux/arm/vpx_config.c
+++ source/config/linux/arm/vpx_config.c
@@ -1,2 +1,2 @@
-static const char* const cfg = "--target=armv6-linux-gcc --enable-pic --disable-postproc --disable-install-docs --disable-examples --disable-install-srcs";
+static const char* const cfg = "--target=armv6-linux-gcc --enable-pic --disable-postproc --disable-install-docs --disable-install-srcs";
 const char *vpx_codec_build_config(void) {return cfg;}
