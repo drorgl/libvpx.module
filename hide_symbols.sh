3f8b70bc2cc6547633d9cf8c0efaefe3e7d341be
hclam@chromium.org <hclam@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2011-10-24 16:58:38 +0000

    Hide symbols of libvpx for use in chromium
    Modify script and asm file to add support for hidden symbols.
    
    git-svn-id: http://src.chromium.org/svn/trunk/deps/third_party/libvpx@106929 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- hide_symbols.sh
+++ hide_symbols.sh
@@ -9,4 +9,40 @@
 # Run this script after source/libvpx has been replaced with the latest
 # source.
 
+# Patch
+echo "<<EOF
+Index: source/libvpx/vpx_ports/x86_abi_support.asm
+===================================================================
+--- source/libvpx/vpx_ports/x86_abi_support.asm	(revision 106910)
++++ source/libvpx/vpx_ports/x86_abi_support.asm	(working copy)
+@@ -92,6 +92,26 @@
+ %define sym(x) _ %+ x
+ %endif
+ 
++;  PRIVATE
++;  Macro for the attribute to hide a global symbol for the target ABI.
++;
++;  Chromium doesn't like exported global symbols due to symbol clashing with
++;  plugins among other things.
++;
++;  Requires Chromium's patched copy of yasm:
++;    http://src.chromium.org/viewvc/chrome?view=rev&revision=73761
++;    http://www.tortall.net/projects/yasm/ticket/236
++;
++%ifidn   __OUTPUT_FORMAT__,elf32
++%define PRIVATE :hidden
++%elifidn __OUTPUT_FORMAT__,elf64
++%define PRIVATE :hidden
++%elifidn __OUTPUT_FORMAT__,x64
++%define PRIVATE
++%else
++%define PRIVATE :private_extern
++%endif
++
+ ; arg()
+ ; Return the address specification of the given argument
+ ;
+EOF" | patch -p0
+
+# Add PRIVATE directive to all assembly functions.
 find source/libvpx -type f -name '*.asm' | xargs -i sed -i -E 's/^\s*global\s+sym\(([a-zA-Z][a-zA-Z0-9_]*)\)\s*$/global sym(\1) PRIVATE/' {}
