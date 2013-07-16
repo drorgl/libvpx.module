1caff2f5cf4bc45ee1798419784f1416d5787dfe
fgalligan@chromium.org <fgalligan@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2013-07-16 23:24:34 +0000

    libvpx: Pull from upstream
    Fixes a few vp9 bitstream bugs. This CL will affect all current
    VP9 encoded streams.
    
    Current HEAD: 33149cbb4c106495d3b99716f8fc32bb41ddfce1
    
    git log from upstream:
    33149cb Replace generated quant tables with static lookup tables.
    3f45406 Fix above context pointers
    d19ed5f Change to extend full border only when needed
    a801f7a Increase border size from 96 to 160.
    e39bd64 Fixing vp9_get_pred_context_comp_ref_p function.
    
    BUG=259166
    TBR=tomfinegan
    
    Review URL: https://codereview.chromium.org/19445002
    
    git-svn-id: http://src.chromium.org/svn/trunk/deps/third_party/libvpx@211873 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- source/config/linux/arm-neon-cpu-detect/vpx_scale_rtcd.h
+++ source/config/linux/arm-neon-cpu-detect/vpx_scale_rtcd.h
@@ -45,6 +45,9 @@ RTCD_EXTERN void (*vp8_yv12_copy_y)(struct yv12_buffer_config *src_ybc, struct y
 void vp9_extend_frame_borders_c(struct yv12_buffer_config *ybf, int subsampling_x, int subsampling_y);
 #define vp9_extend_frame_borders vp9_extend_frame_borders_c
 
+void vp9_extend_frame_inner_borders_c(struct yv12_buffer_config *ybf, int subsampling_x, int subsampling_y);
+#define vp9_extend_frame_inner_borders vp9_extend_frame_inner_borders_c
+
 void vpx_scale_rtcd(void);
 #include "vpx_config.h"
 
