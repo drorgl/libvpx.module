37fc486eee21129fa42b92b99778b54caf773980
johannkoenig@chromium.org <johannkoenig@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2014-04-10 23:06:49 +0000

    Update build files.
    Need to interlock the two scripts.
    
    TBR=tomfinegan@chromium.org
    
    Review URL: https://codereview.chromium.org/234343002
    
    git-svn-id: http://src.chromium.org/svn/trunk/deps/third_party/libvpx@263116 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- source/config/linux/x64/vp9_rtcd.h
+++ source/config/linux/x64/vp9_rtcd.h
@@ -271,10 +271,6 @@ unsigned int vp9_get_mb_ss_mmx(const int16_t *);
 unsigned int vp9_get_mb_ss_sse2(const int16_t *);
 #define vp9_get_mb_ss vp9_get_mb_ss_sse2
 
-void vp9_get_sse_sum_16x16_c(const uint8_t *src_ptr, int source_stride, const uint8_t *ref_ptr, int ref_stride, unsigned int *sse, int *sum);
-void vp9_get16x16var_sse2(const uint8_t *src_ptr, int source_stride, const uint8_t *ref_ptr, int ref_stride, unsigned int *sse, int *sum);
-#define vp9_get_sse_sum_16x16 vp9_get16x16var_sse2
-
 void vp9_get_sse_sum_8x8_c(const uint8_t *src_ptr, int source_stride, const uint8_t *ref_ptr, int ref_stride, unsigned int *sse, int *sum);
 void vp9_get8x8var_sse2(const uint8_t *src_ptr, int source_stride, const uint8_t *ref_ptr, int ref_stride, unsigned int *sse, int *sum);
 #define vp9_get_sse_sum_8x8 vp9_get8x8var_sse2
